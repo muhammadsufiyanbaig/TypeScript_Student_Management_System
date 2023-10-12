34e40836a3a551e1a14dd55e9cbf85d7f80fc2e0
Muhammad Sufiyan Baig <139132305+muhammadsufiyanbaig@users.noreply.github.com>
2023-10-12 12:47:21 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+class school {
+    addStudent() {
+        this.student.push(stdObj);
+    }
+    constructor(name) {
+        this.student = [];
+        this.name = name;
+    }
+}
+let school1 = new ("Bro School");
+export {};
---
+++ code.ts
@@ -0,0 +1,13 @@
+class school{
+    student: any=[];
+    name: string;
+
+    addStudent(){
+        this.student.push(stdObj)
+    }
+    constructor(name: string){
+        this.name = name;
+
+    }
+}
+let school1 = new ("Bro School");
